4fcb564098b0a273027a908cef5054cbcdca6af6
JoshuaBeals <joshua@camelcase.co>
2023-07-03 16:59:36 -0400

    Setup redux persist

--- src/index.js
+++ src/index.js
@@ -4,14 +4,49 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 import { Provider } from "react-redux";
-import store from "./redux/store";
+// import store from "./redux/store";
 import 'react-toastify/dist/ReactToastify.css';
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/lib/integration/react";
+import userReducer from './redux/reducers/userReducer';
+import { configureStore } from '@reduxjs/toolkit';
+
+const persistConfig = { key: "root", storage, version: 1 };
+const persistedReducer = persistReducer(persistConfig, userReducer);
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoreActions: [
+                    FLUSH,
+                    REHYDRATE,
+                    PAUSE,
+                    PERSIST,
+                    PURGE,
+                    REGISTER,
+                ],
+            },
+        }),
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <App />
+            <PersistGate loading={null} persistor={persistStore(store)}>
+                <App />
+            </PersistGate>
         </Provider>
     </React.StrictMode>
 );
